0b0d251caa3583cba2e66e4121d38223f7eb9d56
Sabri Ayeş <sabri@naylalabs.com>
2023-01-28 20:07:58 +0300

    fix: remove unused example codes

--- src/app/pages/auth/sing-in-page/sing-in-page.component.ts
+++ src/app/pages/auth/sing-in-page/sing-in-page.component.ts
@@ -1,7 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LocalAuthService } from '@nayx/core/abstracts';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
 	selector: 'app-sing-in-page',
@@ -11,24 +9,4 @@ import { HttpClient } from '@angular/common/http';
 	styleUrls: ['./sing-in-page.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SingInPageComponent {
-	http = inject(HttpClient);
-	authService = inject(LocalAuthService);
-
-	constructor() {
-		this.authService
-			.signIn({
-				type: 'email',
-				email: 'berkay@naylalabs.com',
-				password: 'Ucr7tp55*',
-			})
-			.subscribe(() => {
-				this.authService.verifyAccount().subscribe(console.log);
-				this.http
-					.get(
-						'https://dev-api-sales-sense-backend.naylalabs.xyz/auth',
-					)
-					.subscribe(console.log);
-			});
-	}
-}
+export class SingInPageComponent {}
